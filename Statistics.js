8cbb5ff91a5ff5ba4c21b3520a4ec3b0e42ff876
Kinga Nowakowska <k.nowakowska.013@gmail.com>
2020-07-09 13:53:15 +0200

    create class Statistics

---
+++ Statistics.js
@@ -0,0 +1,18 @@
+class Statistics{
+    constructor(){
+        this.gameResults=[];
+    }
+    addGameToStatistics(win,bid){
+        let gameResult={win:win, bid:bid}; //można napisać {win,bid}
+        this.gameResults.push(gameResult);
+    }
+    showGameStatistics(){
+        //[liczbGier,liczbaWygr,liczbaPor]
+        let games=this.gameResults.length;
+        let wins = this.gameResults.filter(result =>result.win).length;
+    
+        return [games,wins,games-wins];
+    }
+
+}
+const stac = new Statistics();
