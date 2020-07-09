1f154cff04abdad424584ff975434bfdf581429c
Kinga Nowakowska <k.nowakowska.013@gmail.com>
2020-07-09 14:04:56 +0200

    create class Draw

---
+++ Draw.js
@@ -0,0 +1,15 @@
+class Draw{
+    constructor(){
+        this.options=['#25F24E', 'royalblue', '#F24E25'];
+        let _result = this.drawResult();
+        this.getDrawResult = ()=>  _result;
+    }
+    drawResult(){
+        let colors=[];
+        for(let i=0; i<this.options.length; i++){
+            const index = Math.floor(Math.random()*this.options.length);
+            colors.push(this.options[index]);
+        }
+        return colors;
+    }
+}
